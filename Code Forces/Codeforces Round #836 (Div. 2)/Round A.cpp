bd163348025931ff0ca9142ba898913d71c65d39
Ronit Prasad <55261813+RonitPrasad1@users.noreply.github.com>
2022-11-25 19:57:49 -0600

    Update Round A.cpp

--- Code Forces/Codeforces Round #836 (Div. 2)/Round A.cpp
+++ Code Forces/Codeforces Round #836 (Div. 2)/Round A.cpp
@@ -1,1 +1,34 @@
+/*
+    Competition: Codeforces Round #836 (Div. 2)
+    Problem: "A: SSeeeeiinngg DDoouubbllee"
+    Date: 11/25/2022
+*/
 
+// Header Files:
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+// Driver Function:
+auto main (int argc, const char** argv) -> decltype (0)
+{
+    // FAST IO:
+    std::ios::sync_with_stdio (NULL);
+    std::cin.tie (nullptr);
+    
+    int64_t t;
+    std::cin >> t;
+    
+    std::string PalindromeString, HoldOriginal = " ";
+    while (t--)
+    {
+        std::cin >> PalindromeString;
+        
+        HoldOriginal = PalindromeString;
+        
+        reverse(HoldOriginal.begin(), HoldOriginal.end());
+        std::cout << PalindromeString << HoldOriginal << '\n';
+    }
+    
+    return 0;
+}
