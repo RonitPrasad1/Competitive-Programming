6831c89cd27fd6dca7cccead22d7a58a428f5f6f
Ronit Prasad <55261813+RonitPrasad1@users.noreply.github.com>
2022-11-27 02:22:23 -0600

    Update Combinations.cpp

--- CP Quick Functions/Combinations.cpp
+++ CP Quick Functions/Combinations.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 template <typename DataType>
-inline auto NCR (DataType n, DataType r) -> int64_t;
+inline auto Combinations (DataType n, DataType r) -> int64_t;
 
 // Driver Function:
 auto main (int argc, const char** argv) -> decltype (0)
@@ -12,19 +12,19 @@ auto main (int argc, const char** argv) -> decltype (0)
     
     constexpr static int64_t a = 15, b = 5;
     
-    std::cout << NCR(a, b) << '\n';
+    std::cout << Combinations(a, b) << '\n';
     
     return 0;
 }
 
 template <typename DataType>
-inline auto NCR (DataType n, DataType r) -> int64_t
+inline auto Combinations (DataType n, DataType r) -> int64_t
 {
     if (r == 0) 
         return 1;
 
     if (r > n / 2) 
-        return NCR(n, n - r); 
+        return Combinations(n, n - r); 
 
     int64_t Result = 1; 
     for (int64_t k = 1; k <= r; ++k)
